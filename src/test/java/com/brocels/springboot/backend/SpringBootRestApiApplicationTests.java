9024c15f76fcaa57d8db937966821a99143fa0fc
Brocel <geenens.prof@gmail.com>
2021-05-17 12:11:00 -0300

    First Commit

---
+++ src/test/java/com/brocels/springboot/backend/SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brocels.springboot.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
