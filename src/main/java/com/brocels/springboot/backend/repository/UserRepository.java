a3f3fc243c38d38beba7f0561c9fbf64a3b951a6
Brocel <geenens.prof@gmail.com>
2021-05-17 16:31:36 -0300

    Writing test for model user... part6 : finish writing tests find by

--- src/main/java/com/brocels/springboot/backend/repository/UserRepository.java
+++ src/main/java/com/brocels/springboot/backend/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.brocels.springboot.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.brocels.springboot.backend.model.User;
@@ -7,4 +9,11 @@ import com.brocels.springboot.backend.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	User findByUserName(String userName);
+	
+	User findByEmail(String email);
+	
+	List<User> findByCountry(String country);
+	
+	List<User> findByState(String state);
+	
 }
--- src/test/java/com/brocels/springboot/backend/UserJPAUnitTest.java
+++ src/test/java/com/brocels/springboot/backend/UserJPAUnitTest.java
@@ -33,13 +33,13 @@ public class UserJPAUnitTest {
 	@Test
 	public void should_store_a_user() {
 		
-		User user = repository.save(new User("TestFirstName", "TestLastName", "TestUserName","TestEmail", 666, "TestCountry", "TestState", "TestCity", "TestPassword"));
+		User user = repository.save(new User("TestFirstName", "TestLastName", "TestUserName","TestEmail@test.com", 666, "TestCountry", "TestState", "TestCity", "TestPassword"));
 		
 		assertThat(user).hasFieldOrProperty("id");
 		assertThat(user).hasFieldOrPropertyWithValue("firstName", "TestFirstName");
 		assertThat(user).hasFieldOrPropertyWithValue("lastName", "TestLastName");
 		assertThat(user).hasFieldOrPropertyWithValue("userName", "TestUserName");
-		assertThat(user).hasFieldOrPropertyWithValue("email", "TestEmail");
+		assertThat(user).hasFieldOrPropertyWithValue("email", "TestEmail@test.com");
 		assertThat(user).hasFieldOrPropertyWithValue("age", 666);
 		assertThat(user).hasFieldOrPropertyWithValue("country", "TestCountry");
 		assertThat(user).hasFieldOrPropertyWithValue("state", "TestState");
@@ -51,9 +51,9 @@ public class UserJPAUnitTest {
 	@Test
 	public void should_find_all_users() {
 		
-		User user1 = new User("TestFirstName1", "TestLastName1", "TestUserName1","TestEmail1", 11, "TestCountry1", "TestState1", "TestCity1", "TestPassword1");
-		User user2 = new User("TestFirstName2", "TestLastName2", "TestUserName2","TestEmail2", 22, "TestCountry2", "TestState2", "TestCity2", "TestPassword2");
-		User user3 = new User("TestFirstName3", "TestLastName3", "TestUserName3","TestEmail3", 33, "TestCountry3", "TestState3", "TestCity3", "TestPassword3");
+		User user1 = new User("TestFirstName1", "TestLastName1", "TestUserName1","TestEmail1@test.com", 11, "TestCountry1", "TestState1", "TestCity1", "TestPassword1");
+		User user2 = new User("TestFirstName2", "TestLastName2", "TestUserName2","TestEmail2@test.com", 22, "TestCountry2", "TestState2", "TestCity2", "TestPassword2");
+		User user3 = new User("TestFirstName3", "TestLastName3", "TestUserName3","TestEmail3@test.com", 33, "TestCountry3", "TestState3", "TestCity3", "TestPassword3");
 		
 		Iterable<User> users = repository.findAll();
 		
@@ -63,10 +63,10 @@ public class UserJPAUnitTest {
 	@Test
 	public void should_find_user_by_id() {
 		
-		User user1 = new User("TestFirstName1", "TestLastName1", "TestUserName1","TestEmail1", 11, "TestCountry1", "TestState1", "TestCity1", "TestPassword1");
+		User user1 = new User("TestFirstName1", "TestLastName1", "TestUserName1","TestEmail1@test.com", 11, "TestCountry1", "TestState1", "TestCity1", "TestPassword1");
 		entityManager.persist(user1);
 		
-		User user2 = new User("TestFirstName2", "TestLastName2", "TestUserName2","TestEmail2", 22, "TestCountry2", "TestState2", "TestCity2", "TestPassword2");
+		User user2 = new User("TestFirstName2", "TestLastName2", "TestUserName2","TestEmail2@test.com", 22, "TestCountry2", "TestState2", "TestCity2", "TestPassword2");
 		entityManager.persist(user2);
 		
 		User foundUser = repository.findById(user2.getId()).get();
@@ -78,13 +78,13 @@ public class UserJPAUnitTest {
 	@Test
 	public void should_find_user_by_username() {
 		
-		User user1 = new User("TestFirstName1", "TestLastName1", "TestUserName1","TestEmail1", 11, "TestCountry1", "TestState1", "TestCity1", "TestPassword1");
+		User user1 = new User("TestFirstName1", "TestLastName1", "TestUserName1","TestEmail1@test.com", 11, "TestCountry1", "TestState1", "TestCity1", "TestPassword1");
 		entityManager.persist(user1);
 		
-		User user2 = new User("TestFirstName2", "TestLastName2", "TestUserName2","TestEmail2", 22, "TestCountry2", "TestState2", "TestCity2", "TestPassword2");
+		User user2 = new User("TestFirstName2", "TestLastName2", "TestUserName2","TestEmail2@test.com", 22, "TestCountry2", "TestState2", "TestCity2", "TestPassword2");
 		entityManager.persist(user2);
 		
-		User user3 = new User("TestFirstName3", "TestLastName3", "TestUserName3","TestEmail3", 33, "TestCountry3", "TestState3", "TestCity3", "TestPassword3");
+		User user3 = new User("TestFirstName3", "TestLastName3", "TestUserName3","TestEmail3@test.com", 33, "TestCountry3", "TestState3", "TestCity3", "TestPassword3");
 		entityManager.persist(user3);
 		
 		User foundUser = repository.findByUserName("TestUserName2");
@@ -92,4 +92,56 @@ public class UserJPAUnitTest {
 		assertThat(foundUser).isEqualTo(user2);
 	
 	}
+	
+	@Test
+	public void should_find_user_by_email() {
+		
+		User user1 = new User("TestFirstName1", "TestLastName1", "TestUserName1","TestEmail1@test.com", 11, "TestCountry1", "TestState1", "TestCity1", "TestPassword1");
+		entityManager.persist(user1);
+		
+		User user2 = new User("TestFirstName2", "TestLastName2", "TestUserName2","TestEmail2@test.com", 22, "TestCountry2", "TestState2", "TestCity2", "TestPassword2");
+		entityManager.persist(user2);
+		
+		User user3 = new User("TestFirstName3", "TestLastName3", "TestUserName3","TestEmail1@test.com", 33, "TestCountry3", "TestState3", "TestCity3", "TestPassword3");
+		entityManager.persist(user3);
+		
+		User user = repository.findByEmail("TestEmail2@test.com");
+		
+		assertThat(user).isEqualTo(user2);
+	}
+	
+	@Test
+	public void should_find_users_by_country() {
+		
+		User user1 = new User("TestFirstName1", "TestLastName1", "TestUserName1","TestEmail1", 11, "TestCountry1", "TestState1", "TestCity1", "TestPassword1");
+		entityManager.persist(user1);
+		
+		User user2 = new User("TestFirstName2", "TestLastName2", "TestUserName2","TestEmail2", 22, "TestCountry2", "TestState2", "TestCity2", "TestPassword2");
+		entityManager.persist(user2);
+		
+		User user3 = new User("TestFirstName3", "TestLastName3", "TestUserName3","TestEmail3", 33, "TestCountry1", "TestState3", "TestCity3", "TestPassword3");
+		entityManager.persist(user3);
+		
+		Iterable<User> usersByCountry = repository.findByCountry("TestCountry1");
+		
+		assertThat(usersByCountry).hasSize(2).contains(user1, user3);
+	}
+	
+	@Test
+	public void should_find_users_by_state() {
+		
+		User user1 = new User("TestFirstName1", "TestLastName1", "TestUserName1","TestEmail1", 11, "TestCountry1", "TestState1", "TestCity1", "TestPassword1");
+		entityManager.persist(user1);
+		
+		User user2 = new User("TestFirstName2", "TestLastName2", "TestUserName2","TestEmail2", 22, "TestCountry2", "TestState2", "TestCity2", "TestPassword2");
+		entityManager.persist(user2);
+		
+		User user3 = new User("TestFirstName3", "TestLastName3", "TestUserName3","TestEmail3", 33, "TestCountry2", "TestState2", "TestCity3", "TestPassword3");
+		entityManager.persist(user3);
+		
+		Iterable<User> usersByState = repository.findByState("TestState2");
+		
+		assertThat(usersByState).hasSize(2).contains(user2, user3);
+	}
+	
 }
