f97e79deb850ffe531ba8f69d96f574c4637ac7d
Brocel <geenens.prof@gmail.com>
2021-05-17 14:30:20 -0300

    Writing test for model user... part2 : hasfieldorpropertywithvalue

--- src/test/java/com/brocels/springboot/backend/model/UserJPAUnitTest.java
+++ src/test/java/com/brocels/springboot/backend/model/UserJPAUnitTest.java
@@ -29,6 +29,23 @@ public class UserJPAUnitTest {
 		assertThat(users).isEmpty();
 	}
 	
+	@Test
+	public void should_store_a_user() {
+		
+		User user = repository.save(new User("TestFirstName", "TestLastName", "TestUserName","TestEmail", 666, "TestCountry", "TestState", "TestCity", "TestPassword"));
+		
+		assertThat(user).hasFieldOrProperty("id");
+		assertThat(user).hasFieldOrPropertyWithValue("first_name", "TestFirstName");
+		assertThat(user).hasFieldOrPropertyWithValue("last_name", "TestLastName");
+		assertThat(user).hasFieldOrPropertyWithValue("user_name", "TestUserName");
+		assertThat(user).hasFieldOrPropertyWithValue("email", "TestEmail");
+		assertThat(user).hasFieldOrPropertyWithValue("age", 666);
+		assertThat(user).hasFieldOrPropertyWithValue("country", "TestCountry");
+		assertThat(user).hasFieldOrPropertyWithValue("state", "TestState");
+		assertThat(user).hasFieldOrPropertyWithValue("city", "TestCity");
+		assertThat(user).hasFieldOrPropertyWithValue("password", "TestPassword");
+	
+	}
 	
 	
 	
