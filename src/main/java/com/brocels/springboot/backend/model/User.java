fc4892bcad31acc5b51033c392a9580df0524afc
Brocel <geenens.prof@gmail.com>
2021-05-17 13:15:26 -0300

    Edit User : add constructor using fields + getters & setters

--- src/main/java/com/brocels/springboot/backend/model/User.java
+++ src/main/java/com/brocels/springboot/backend/model/User.java
@@ -42,4 +42,91 @@ public class User {
     public User() {
 
     }
+
+	public User(String firstName, String lastName, String userName, String email, int age, String country, String state,
+			String city) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.email = email;
+		this.age = age;
+		this.country = country;
+		this.state = state;
+		this.city = city;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+    
+    
 }
