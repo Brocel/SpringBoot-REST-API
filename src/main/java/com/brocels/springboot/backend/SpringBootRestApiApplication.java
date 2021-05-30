d02ea763690b8a9b9d5c3a7143a177bbc3e290b5
Brocel <geenens.prof@gmail.com>
2021-05-30 15:06:55 -0300

    Edit run  behaviour, inserting a new fakeUser at each run

--- src/main/java/com/brocels/springboot/backend/SpringBootRestApiApplication.java
+++ src/main/java/com/brocels/springboot/backend/SpringBootRestApiApplication.java
@@ -1,5 +1,7 @@
 package com.brocels.springboot.backend;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,7 +23,15 @@ public class SpringBootRestApiApplication implements CommandLineRunner{
 	@Override
 	public void run(String...args) throws Exception {
 		
-		this.userRepository.save(new User("Fake", "User", "FakeUser", "fake.user@test.com", 666, "Country", "State", "City", "FakePWD666!"));
+		User fakeUser = userRepository.findByEmail("fake.user@test.com");
+		
+		if ( fakeUser == null ) {
+			this.userRepository.save(new User("Fake", "User", "FakeUser", "fake.user@test.com", 666, "FakeCountry", "State", "City", "FakePWD666!"));
+		} else {
+			this.userRepository.delete(fakeUser);
+			this.userRepository.save(new User("Fake", "User", "FakeUser", "fake.user@test.com", 666, "FakeCountry", "State", "City", "FakePWD666!"));
+		}
+		
 	}
 
 }
