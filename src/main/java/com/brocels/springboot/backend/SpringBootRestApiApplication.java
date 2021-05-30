260fe65123c1c27d28e498741f2222f570ce8452
Brocel <geenens.prof@gmail.com>
2021-05-30 12:10:01 -0300

    Edit run to populate DB with a fake user

--- src/main/java/com/brocels/springboot/backend/SpringBootRestApiApplication.java
+++ src/main/java/com/brocels/springboot/backend/SpringBootRestApiApplication.java
@@ -1,13 +1,27 @@
 package com.brocels.springboot.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootRestApiApplication {
+import com.brocels.springboot.backend.model.User;
+import com.brocels.springboot.backend.repository.UserRepository;
 
+@SpringBootApplication
+public class SpringBootRestApiApplication implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApiApplication.class, args);
+	}
+	
+	@Override
+	public void run(String...args) throws Exception {
+		
+		this.userRepository.save(new User("Fake", "User", "FakeUser", "fake.user@test.com", 666, "Country", "State", "City", "FakePWD666!"));
 	}
 
 }
