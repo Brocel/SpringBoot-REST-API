ac18147a6c85ac362cb99eab52537efd4b7bf4b8
Brocel <geenens.prof@gmail.com>
2021-06-01 10:40:09 -0300

    Put Mapping

--- src/main/java/com/brocels/springboot/backend/controller/UserController.java
+++ src/main/java/com/brocels/springboot/backend/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.brocels.springboot.backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +30,24 @@ public class UserController {
 		return this.userRepository.findAll();
 	}
 	
+	@PutMapping("users/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+		Optional<User> userData = userRepository.findById(id);
+		
+		if(userData.isPresent()) {
+			User _user = userData.get();
+			_user.setFirstName(user.getFirstName());
+			_user.setLastName(user.getLastName());
+			_user.setUserName(user.getUserName());
+			_user.setEmail(user.getEmail());
+			_user.setAge(user.getAge());
+			_user.setCountry(user.getCountry());
+			_user.setState(user.getState());
+			_user.setCity(user.getCity());
+			_user.setPassword(user.getPassword());
+			return new ResponseEntity<>(userRepository.save(_user), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 }
