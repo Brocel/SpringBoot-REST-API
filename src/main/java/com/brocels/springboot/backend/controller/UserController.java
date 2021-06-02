7b353421faa1cd29457b82ec16d8fc78cf55e60d
Brocel <geenens.prof@gmail.com>
2021-06-02 12:01:30 -0300

    Create Delete Mapping users/{id}

--- src/main/java/com/brocels/springboot/backend/controller/UserController.java
+++ src/main/java/com/brocels/springboot/backend/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -87,6 +88,16 @@ public class UserController {
 			return new ResponseEntity<>(userRepository.save(_user), HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("users/{id}")
+	public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") long id) {
+		try {
+			userRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
