cb4ab375eafbb2e434338945b45438e8a30f1e82
Brocel <geenens.prof@gmail.com>
2021-06-09 14:53:58 -0300

    edit customuserdetailsservice

--- src/main/java/com/brocels/springboot/backend/service/CustomUserDetailsService.java
+++ src/main/java/com/brocels/springboot/backend/service/CustomUserDetailsService.java
@@ -1,15 +1,26 @@
 package com.brocels.springboot.backend.service;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.brocels.springboot.backend.model.User;
+import com.brocels.springboot.backend.repository.UserRepository;
+
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private UserRepository repository;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		User user = repository.findByUserName(username);
+		return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), new ArrayList<>());
 	}
 
 }
