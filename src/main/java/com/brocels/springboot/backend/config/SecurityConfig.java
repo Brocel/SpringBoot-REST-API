ce1a45348f8b69751c0d78193f5040f3ac20fe24
Brocel <geenens.prof@gmail.com>
2021-06-09 12:49:16 -0300

    edit websecurity config, create JwtFilter, JwtUtil

--- src/main/java/com/brocels/springboot/backend/config/SecurityConfig.java
+++ src/main/java/com/brocels/springboot/backend/config/SecurityConfig.java
@@ -1,5 +1,20 @@
 package com.brocels.springboot.backend.config;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.brocels.springboot.backend.service.CustomUserDetailsService;
+import com.brocels.springboot.backend.util.JwtFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtFilter jwtFilter;
 }
