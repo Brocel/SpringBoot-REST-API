22283d18a2fe8766bdd8c00ac4b59d72d641ac18
Brocel <geenens.prof@gmail.com>
2021-06-09 12:40:00 -0300

    edit authrequest : no contructor

--- src/main/java/com/brocels/springboot/backend/util/AuthRequest.java
+++ src/main/java/com/brocels/springboot/backend/util/AuthRequest.java
@@ -1,5 +1,14 @@
 package com.brocels.springboot.backend.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequest {
 
+	private String userName;
+	private String password;
 }
