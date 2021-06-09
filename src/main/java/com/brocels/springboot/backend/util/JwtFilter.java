9ed3db3d4a8e6a2b54ed355462276d2508b87dff
Brocel <geenens.prof@gmail.com>
2021-06-09 12:51:39 -0300

    edit JwtFilter : building the filter

--- src/main/java/com/brocels/springboot/backend/util/JwtFilter.java
+++ src/main/java/com/brocels/springboot/backend/util/JwtFilter.java
@@ -1,5 +1,33 @@
 package com.brocels.springboot.backend.util;
 
-public class JwtFilter {
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.brocels.springboot.backend.service.CustomUserDetailsService;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter{
+
+	
+	@Autowired
+    private JwtUtil jwtUtil;
+	
+    @Autowired
+    private CustomUserDetailsService service;
+    
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
