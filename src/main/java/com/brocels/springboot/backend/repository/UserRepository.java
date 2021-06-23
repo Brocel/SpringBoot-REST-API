601758a24bb63e53533422f7085ae47b2625685a
Brocel <geenens.prof@gmail.com>
2021-06-23 14:28:20 -0300

    editing User repo

--- src/main/java/com/brocels/springboot/backend/repository/UserRepository.java
+++ src/main/java/com/brocels/springboot/backend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.brocels.springboot.backend.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,12 +9,16 @@ import com.brocels.springboot.backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	User findByUserName(String userName);
+	Optional<User> findByUserName(String userName);
 	
 	User findByEmail(String email);
 	
 	List<User> findByCountry(String country);
 	
 	List<User> findByState(String state);
+	
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
 	
 }
