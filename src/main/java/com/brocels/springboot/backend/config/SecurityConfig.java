dcf0294ad567f3115a0cf16df78e2b851e1bd98a
Brocel <geenens.prof@gmail.com>
2021-06-23 11:13:05 -0300

    test jwt: CORS issues

--- src/main/java/com/brocels/springboot/backend/config/SecurityConfig.java
+++ src/main/java/com/brocels/springboot/backend/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http.cors();
     }
 }
